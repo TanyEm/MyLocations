d74d399a6c69b760b22fe85618861f12b171ffd5
Tanya Tomchuk <tanyem@protonmail.com>
2017-05-26 14:00:12 +0300

    Added storing the locations with Core Data

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Tanya Tomchuk on 26.05.17.
+//  Copyright © 2017 Tanya Tomchuk. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
