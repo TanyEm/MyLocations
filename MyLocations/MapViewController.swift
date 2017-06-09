ff812bb0110aa10a6cb58639438f2dad6f72236a
Tanya Tomchuk <tanyem@protonmail.com>
2017-06-09 16:12:10 +0300

    Changed color for location points on the map Added button in annotation the location point

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -55,6 +55,13 @@ class MapViewController: UIViewController {
         mapView.addAnnotations(locations)
     }
     
+    
+    // The method takes one parameter, sender, that refers to the control that sent the action message.
+    // In this case the sender will be the (i) button.
+    func showLocationDetails(_ sender: UIButton) {
+        
+    }
+    
     func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
         let region: MKCoordinateRegion
         
@@ -100,4 +107,48 @@ class MapViewController: UIViewController {
 
 extension MapViewController: MKMapViewDelegate {
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Because MKAnnotation is a protocol, there may be other objects apart from the Location object 
+        // that wants to be annotations on the map. An example is the blue dot that represents the user’s current location.
+        guard annotation is Location else {
+            return nil
+        }
+        // This looks similar to creating a table view cell.You ask the map view to re-use 
+        // an annotation view object. If it cannot find a recyclable annotation view, then you create a new one.
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            // This sets some properties to configure the look and feel of the annotation view. 
+            // Previously the pins were red, but you make them green here.
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop = false
+            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+            
+            // You create a new UIButton object that looks like a detail disclosure button.
+            let rightButton = UIButton(type: .detailDisclosure)
+            //  You use the target-action pattern to hook up the button’s “Touch Up Inside” event with a new method showLocationDetails()
+            rightButton.addTarget(self, action: #selector(showLocationDetails), for: .touchUpInside)
+            // Add the button to the annotation view’s accessory view.
+            pinView.rightCalloutAccessoryView = rightButton
+            annotationView = pinView
+        }
+            
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            
+            // Once the annotation view is constructed and configured, you obtain a reference to that detail disclosure button
+            // again and set its tag to the index of the Location object in the locations array. That way you can find 
+            // the Location object later in showLocationDetails() when the button is pressed.
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            
+            if let index = locations.index(of: annotation as! Location) {
+                button.tag = index
+            }
+        }
+        return annotationView
+    }
 }
