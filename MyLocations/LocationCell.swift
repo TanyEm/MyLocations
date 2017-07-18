fe310e7a4897fe4a0a35c24538360b98e86f11ea
Tanya Tomchuk <tanyem@protonmail.com>
2017-07-18 14:08:35 +0300

    Added image for locations on the Locations screen

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -12,6 +12,7 @@ class LocationCell: UITableViewCell {
     
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +23,13 @@ class LocationCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func thumbnail(for location: Location) -> UIImage {
+        if location.hasPhoto, let image = location.photoImage {
+            return image
+        }
+        return UIImage()
     }
     
     func configure(for location: Location) {
@@ -46,6 +54,6 @@ class LocationCell: UITableViewCell {
             addressLabel.text = String(format:
                 "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
         }
+        photoImageView.image = thumbnail(for: location)
     }
-
 }
